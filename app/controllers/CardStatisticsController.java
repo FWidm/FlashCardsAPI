1720a0b8484dd0ba241db9b74ab4b6f4d77aff49
FWidm <fabian.widmann@uni-ulm.de>
2017-02-20 18:13:22 +0100

    Fixed POST on cardStatistics. If a date is sent that is AFTER the servers time we set the time as null currently.

--- app/controllers/CardStatisticsController.java
+++ app/controllers/CardStatisticsController.java
@@ -1,12 +1,15 @@
 package controllers;
 
 import models.statistics.CardStatistics;
+import play.mvc.BodyParser;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Security;
 import repositories.CardStatisticsRepository;
 import util.ActionAuthenticator;
+import util.JsonKeys;
 import util.JsonUtil;
+import util.exceptions.InvalidInputException;
 import util.exceptions.NotAuthorizedException;
 
 import java.text.ParseException;
@@ -25,9 +28,25 @@ public class CardStatisticsController extends Controller {
         } catch (NotAuthorizedException e) {
             return unauthorized(JsonUtil.prepareJsonStatus(UNAUTHORIZED, e.getMessage()));
         } catch (ParseException e) {
-            return badRequest(JsonUtil.prepareJsonStatus(BAD_REQUEST, "Error while parsing count number. Please provide a valid number."));
+            return badRequest(JsonUtil.prepareJsonStatus(BAD_REQUEST, "Error while parsing count number." +
+                    " Please provide a valid number."));
         }
     }
 
 
+    @Security.Authenticated(ActionAuthenticator.class)
+    @BodyParser.Of(BodyParser.Json.class)
+    public Result addCardStatistics() {
+        CardStatistics cardStatistics;
+        try {
+            cardStatistics = CardStatisticsRepository.createCardStatistics(request().username(),  request().body().asJson());
+        } catch (ParseException e) {
+            return badRequest(JsonUtil.prepareJsonStatus(BAD_REQUEST, "Error while parsing date. " +
+                    "Dates should have the format: '"+ JsonKeys.DATE_FORMAT+"'. Example: '"+JsonKeys.DATE_START+"':'2017-02-10 13:24:29 UTC'. Applies to: '"+JsonKeys.DATE_START+"' and '"+JsonKeys.DATE_END+"'."));
+        } catch (InvalidInputException e) {
+            e.printStackTrace();
+            return badRequest(JsonUtil.prepareJsonStatus(BAD_REQUEST,e.getMessage()));
+        }
+        return ok(JsonUtil.toJson(cardStatistics));
+    }
 }
--- app/repositories/CardStatisticsRepository.java
+++ app/repositories/CardStatisticsRepository.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import models.FlashCard;
 import models.User;
 import models.statistics.CardStatistics;
+import play.Logger;
 import play.api.mvc.Flash;
 import util.JsonKeys;
 import util.RequestKeys;
@@ -50,21 +51,25 @@ public class CardStatisticsRepository {
         return statistics;
     }
 
+    /**
+     * Creates one new CardStatistics object
+     * @param email of the authenticated user
+     * @param json content of the new object
+     * @return new object or exception
+     * @throws InvalidInputException if some of the required attributes are missing
+     * @throws ParseException if we cannot retrieve the date from the datestring.
+     */
     public static CardStatistics createCardStatistics(String email, JsonNode json) throws InvalidInputException, ParseException {
         CardStatistics cardStatistics;
-        User user=null;
+        User user=UserRepository.findUserByEmail(email);;
         FlashCard card=null;
         float knowledge=Float.MIN_VALUE;
         int drawer= Integer.MIN_VALUE;
         Date startDate=null, endDate = null;
 
-        if (json.has(JsonKeys.STATISTICS_USER)) {
-            if(json.get(JsonKeys.MESSAGE_RECIPIENT).has(JsonKeys.USER_ID))
-                user = UserRepository.findById(json.get(JsonKeys.MESSAGE_RECIPIENT).get(JsonKeys.USER_ID).asLong());
-        }
 
         if (json.has(JsonKeys.STATISTICS_CARD)) {
-            if(json.get(JsonKeys.MESSAGE_RECIPIENT).has(JsonKeys.FLASHCARD_ID))
+            if(json.get(JsonKeys.STATISTICS_CARD).has(JsonKeys.FLASHCARD_ID))
                 card=FlashCardRepository.getFlashCard(json.get(JsonKeys.STATISTICS_CARD).get(JsonKeys.FLASHCARD_ID).asLong());
         }
 
@@ -73,25 +78,40 @@ public class CardStatisticsRepository {
         }
 
         if(json.has(JsonKeys.DATE_START)){
-            String textDate = json.get(JsonKeys.DATE_START).asText();;
+            String textDate = json.get(JsonKeys.DATE_START).asText();
             DateFormat format = new SimpleDateFormat(JsonKeys.DATE_FORMAT);
             startDate = format.parse(textDate);
+            // TODO: 20.02.2017 decide if we want to invalidate sent dates.
+            /*if(startDate.after(new Date()))
+                startDate=null;*/
         }
 
         if(json.has(JsonKeys.DATE_END)){
             String textDate = json.get(JsonKeys.DATE_START).asText();;
             DateFormat format = new SimpleDateFormat(JsonKeys.DATE_FORMAT);
             endDate = format.parse(textDate);
+            // TODO: 20.02.2017 decide if we want to invalidate sent dates.
+            /*if(endDate.after(new Date()))
+                endDate=null;*/
         }
 
         if(json.has(JsonKeys.STATISTICS_DRAWER)){
             drawer=json.get(JsonKeys.STATISTICS_DRAWER).asInt();
         }
 
-        if(user==null || card==null || startDate==null || endDate==null|| drawer < 0 || knowledge < 0 ){
-            throw new InvalidInputException("Please provide all neccessary details to create a new CardStatistic such " +
-                    "as: ("+JsonKeys.STATISTICS_CARD+", "+JsonKeys.STATISTICS_DRAWER+", "+JsonKeys.STATISTICS_KNOWLEDGE+
-                    ", "+JsonKeys.STATISTICS_USER+" and two timestamps: "+JsonKeys.DATE_START+", "+ JsonKeys.DATE_END+" in the format: "+JsonKeys.DATE_FORMAT+").");
+        Logger.debug("user="+user);
+        Logger.debug("card="+card);
+        Logger.debug("startDate="+startDate);
+        Logger.debug("endDate="+endDate);
+        Logger.debug("knowledge="+knowledge);
+        Logger.debug("drawer="+drawer);
+
+        if(card==null)
+            throw new InvalidInputException("The requested card does not exist. No CardStatistics-object was created.");
+        if(user==null || drawer < 0 || knowledge < 0 ){
+            throw new InvalidInputException("Please provide all necessary details to create a new CardStatistic such " +
+                    "as: ("+JsonKeys.STATISTICS_CARD+", "+JsonKeys.STATISTICS_DRAWER+" (positive), "+JsonKeys.STATISTICS_KNOWLEDGE+
+                    "(positive, float) and two timestamps: "+JsonKeys.DATE_START+", "+ JsonKeys.DATE_END+" in the format: "+JsonKeys.DATE_FORMAT+").");
         }
 
         cardStatistics=new CardStatistics(user,card,knowledge,drawer,startDate,endDate);
